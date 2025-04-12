3c5eca85695b71fa4c0e0e6b05f2d1ef09937240
Jumballaya <nchupacabra@gmail.com>
2025-04-12 14:42:10 -0500

    v1.0.1 - exposing types now

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 export * from "./BufferWrap";
 export * from "./strategies/ArrayBufferStrategy";
+export * from "./BaseStrategy";
+export type {
+  ProxyShape,
+  BufferType,
+  BufferStrategy,
+  ProxyAccessStrategy,
+  StrategyConfig,
+  WrapperConfig,
+  WrapperStructConfig,
+  BufferList,
+  CopyTarget,
+} from "./types";
