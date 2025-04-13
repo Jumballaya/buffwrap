2f9cfa111ce8bcc4cb0aeb6eaa61185f7f5d0636
Jumballaya <nchupacabra@gmail.com>
2025-04-12 23:39:39 -0500

    Fix external typing

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,9 @@ export * from "./BufferWrap";
 export * from "./strategies/ArrayBufferStrategy";
 export * from "./BaseStrategy";
 export type {
-  ProxyShape,
   BufferType,
   BufferStrategy,
-  ProxyAccessStrategy,
   StrategyConfig,
   WrapperConfig,
   WrapperStructConfig,
-  BufferList,
-  CopyTarget,
 } from "./types";
